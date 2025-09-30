17f3f598bb7428e6f0a9c7d86693ba83c5db018e
Brian Cox <bcox@captechventures.com>
2025-09-30 11:22:00 -0400

    added logout

--- app.py
+++ app.py
@@ -1678,4 +1678,4 @@ def toggle_user_active(user_id):
     return redirect(url_for('admin_users'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5002)
